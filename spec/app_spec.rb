2aea280533c2d8bee59df04f694af9cd6a3ec428
Eric J. Holmes <eric@ejholmes.net>
2014-03-26 16:38:41 -0700

    Cleanup.

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe App do
-  use_vcr_cassette
   include Rack::Test::Methods
 
   def app
@@ -12,7 +11,9 @@ describe App do
     header 'Content-Type', 'application/x-www-form-urlencoded'
   end
 
-  it 'creates a slack attachment' do
-    post '/?account=test&token=1234', File.read(File.expand_path('../fixtures/alert.txt', __FILE__))
+  describe 'librato' do
+    it 'sends a message', :vcr do
+      post '/?account=test&token=1234', File.read(File.expand_path('../fixtures/alert.txt', __FILE__))
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,7 @@ VCR.config do |config|
     allow_unused_http_interactions: false,
   }
   config.stub_with :webmock
+  config.configure_rspec_metadata!
 end
 
 RSpec.configure do |config|
@@ -23,4 +24,5 @@ RSpec.configure do |config|
   config.filter_run :focus => true
   config.run_all_when_everything_filtered = true
   config.extend VCR::RSpec::Macros
+  config.treat_symbols_as_metadata_keys_with_true_values = true
 end
