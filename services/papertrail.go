b48741e16f2848d044c8d8c457b9c322c4092d5c
Eric J. Holmes <eric@ejholmes.net>
2014-03-29 02:40:18 -0700

    WIP.

--- services/papertrail.go
+++ services/papertrail.go
@@ -1,29 +1,10 @@
 package services
 
 import(
-	"fmt"
-	"net/http"
 	"github.com/remind101/slack-hooks/slack"
 )
 
-func Papertrail(res http.ResponseWriter, req *http.Request) string {
-	account := req.FormValue("account")
-	token   := req.FormValue("token")
-	//channel := req.FormValue("channel")
-
-	payload := slack.Payload{
-		"Papertrail",
-		"Search",
-		nil,
-	}
-
-	response, err := slack.Post(account, token, payload)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	fmt.Println(response)
-
-	return "Papertrail"
+func Papertrail(payload *slack.Payload) {
+	payload.Username = "Papertrail"
+	payload.Text	 = "Search"
 }
--- slack-hooks.go
+++ slack-hooks.go
@@ -1,12 +1,24 @@
 package main
 
 import(
+	"net/http"
 	"github.com/codegangsta/martini"
 	"github.com/remind101/slack-hooks/services"
+	"github.com/remind101/slack-hooks/slack"
 )
 
 func main() {
 	m := martini.Classic()
+
+	m.Use(func(res http.ResponseWriter, req *http.Request, c martini.Context) {
+		payload := slack.Payload{}
+		c.Map(&payload)
+		c.Next()
+
+		account := req.FormValue("account")
+		token   := req.FormValue("token")
+		slack.Post(account, token, payload)
+	})
 
 	m.Post("/librato",    services.Librato)
 	m.Post("/papertrail", services.Papertrail)
